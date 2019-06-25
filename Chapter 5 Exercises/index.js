d10bb455a40da45ac7c1556100a6dadc815d6b1c
kanderson425 <kyleanderson.dev@gmail.com>
2019-06-24 19:18:04 -0500

    Adds flattening exercise from Eloquent JavaScript book chapter 5

---
+++ Chapter 5 Exercises/index.js
@@ -0,0 +1,16 @@
+//flattening
+var arrays = [[1,2,3],[4,5],[6],[7,8,9]]
+
+console.log(arrays.reduce(function(flat, concat) {
+    return flat.concat(current);
+}, []));
+
+
+//Your Own loop
+
+
+///Everything
+
+
+//Dominant Writing Direction
+
